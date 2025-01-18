4f3156cf8e3674ac9bc7c39681b5e79972467298
Aicha Bnousinane <2314791@crosemont.qc.ca>
2025-01-17 22:36:46 -0500

    Add custom first page with button to create a document

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,26 @@
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { useUser } from '@clerk/clerk-react';
+import { PlusCircle } from 'lucide-react';
+import Image from 'next/image';
+
 const DocumentsPage = () => {
+    const {user} = useUser()
+
     return (
-        <div>
-        This is a protected page
+        <div className="flex flex-col justify-center items-center h-full space-y-4">        
+            <Image className="dark:hidden" src='/empty.png' alt="Empty" width={300} height={300}/>
+            <Image className="hidden dark:block" src='/empty-dark.png' alt="Empty" width={300} height={300}/>
+
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos;s Zotion
+            </h2>
+      
+            <Button>
+                <PlusCircle className="w-4 h-4 mr-2"/>
+                Create a note
+            </Button>
         </div>
     );
 }
