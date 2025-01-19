0de9e60baedb166fb2658450a5f92a0927fc8597
Aicha Bnousinane <2314791@crosemont.qc.ca>
2025-01-19 15:45:58 -0500

    Add user settings

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import { ChevronsLeft, MenuIcon } from "lucide-react"
+import { ChevronsLeft, MenuIcon, User } from "lucide-react"
 import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import {useMediaQuery} from "usehooks-ts"
+import { UserItem } from "./user-item"
 
 export const Navigation = () => {
     const pathname = usePathname()
@@ -100,7 +101,7 @@ export const Navigation = () => {
                     <ChevronsLeft className="w-6 h-6"/>
                 </div>
                 <div>
-                    <p>Action items</p>
+                    <UserItem/>
                 </div>
                 <div className="mt-4">
                     <p>Documents</p>
