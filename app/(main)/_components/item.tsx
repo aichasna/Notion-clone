48958659db277e56bdac02f5c1a8a5540e220160
Aicha Bnousinane <2314791@crosemont.qc.ca>
2025-01-21 22:50:56 -0500

    Add document list

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,8 +1,13 @@
 "use client"
 
+import { Skeleton } from "@/components/ui/skeleton"
+import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
 import { cn } from "@/lib/utils"
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react"
+import { useMutation } from "convex/react"
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
 
 interface ItemProps {
     id?:Id<"documents">
@@ -28,12 +33,32 @@ export const Item = ({
     level=0,
     onExpand,expanded
 }:ItemProps) => {
-    const ChevronIcon = expanded ? ChevronDown : ChevronRight
+    const create = useMutation(api.documents.create)
+    const router = useRouter()
 
     const handleExpand = (event:React.MouseEvent<HTMLDivElement>) => {
         event.stopPropagation()
         onExpand?.()
     }
+
+    const onCreate = (event:React.MouseEvent<HTMLDivElement,MouseEvent>) => {
+        event.stopPropagation()
+        if (!id) return
+        const promise = create({title:"Untitled",parentDocument:id})
+        .then((documentId) => {
+          if (!expanded) {
+            onExpand?.()
+          }
+        })
+    
+        toast.promise(promise,{
+          loading:'Creating a new note...',
+          success:'New note created!',
+          error:'Failed to create a new note'
+        })
+      }
+
+    const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
     return (
         <div 
@@ -68,6 +93,19 @@ export const Item = ({
                 <span className="text-xs">⌘</span>K
                 </kbd>
             )}
+            <div className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+            role="button" onClick={onCreate}>
+                <Plus className="w-4 h-4 text-muted-foreground"/>
+            </div>
+        </div>
+    )
+}
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+    return (
+        <div className="flex gap-x-2 py-[3px]" style={{paddingLeft:level ? `${(level * 12) + 25}px`: '12px'}}>
+            <Skeleton className="w-4 h-4"/>
+            <Skeleton className="w-4 h-[30%]"/>
         </div>
     )
 }
