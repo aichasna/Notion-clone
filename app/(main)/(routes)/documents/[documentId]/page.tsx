2b7330089eac18f6eb4ccf595713c794c1df0593
Aicha Bnousinane <2314791@crosemont.qc.ca>
2025-02-02 16:02:58 -0500

    Add icon and cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,41 @@
-const DocumentPageId = () => {
+"use client"
+
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { Toolbar } from "@/components/toolbar"
+import { Cover } from "@/components/cover"
+
+interface DocumentIdPageProps {
+    params:{
+      documentId:Id<'documents'>
+    }
+}
+
+const DocumentPageId = ( {params}:DocumentIdPageProps ) => {
+    const document = useQuery(api.documents.getById,{
+        documentId:params.documentId
+    })
+
+    if (document === undefined) {
+        return (
+          <div>
+            Loading....
+          </div>
+        )
+    }
+    
+      if (document === null) {
+        return <div>Not Found</div>
+    }    
     return ( 
-        <div>
-            <h1>Document Page</h1>
+        <div className="pb-40">
+            <Cover url={document.coverImage}/>
+            <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
+                <Toolbar initialData={document}/>
+            </div>
         </div>
-     );
+    );
 }
  
 export default DocumentPageId;
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from '../lib/edgestore';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,17 +47,19 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="zotion-theme-2"
-          >
-            <Toaster position="bottom-center" />
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="zotion-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
