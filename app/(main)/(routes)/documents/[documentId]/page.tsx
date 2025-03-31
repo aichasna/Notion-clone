3774fbd03d01a1bcd334542ff5975ddd44801c4d
Aicha Bnousinane <2314791@crosemont.qc.ca>
2025-03-30 21:54:37 -0400

    Adding editor component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,38 +1,61 @@
 "use client"
 
-import { useQuery } from "convex/react"
+import { use } from "react";
+import { useMutation, useQuery } from "convex/react"
 import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
 import { Toolbar } from "@/components/toolbar"
 import { Cover } from "@/components/cover"
+import { Skeleton } from "@/components/ui/skeleton";
+import Editor from "@/components/editor"
 
 interface DocumentIdPageProps {
-    params:{
-      documentId:Id<'documents'>
-    }
+  params: Promise<{
+    documentId: Id<"documents">;
+  }>;
 }
 
 const DocumentPageId = ( {params}:DocumentIdPageProps ) => {
-    const document = useQuery(api.documents.getById,{
-        documentId:params.documentId
-    })
+    const { documentId } = use(params);
+
+    const document = useQuery(api.documents.getById, {
+      documentId,
+    });
+
+    const update = useMutation(api.documents.update);
+    
+    const onChange = (content: string) => {
+      update({
+        id: documentId,
+        content,
+      });
+    };
 
     if (document === undefined) {
-        return (
-          <div>
-            Loading....
+      return (
+        <div>
+          <Cover.Skeleton />
+          <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+            <div className="space-y-4 pl-8 pt-4">
+              <Skeleton className="h-14 w-1/2" />
+              <Skeleton className="h-4 w-4/5" />
+              <Skeleton className="h-4 w-2/5" />
+              <Skeleton className="h-4 w-3/5" />
+            </div>
           </div>
-        )
+        </div>
+      );
     }
     
-      if (document === null) {
-        return <div>Not Found</div>
+    if (document === null) {
+      return <div>Not Found</div>
     }    
     return ( 
         <div className="pb-40">
             <Cover url={document.coverImage}/>
             <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
                 <Toolbar initialData={document}/>
+                <Editor onChange={onChange} initialContent={document.content} />
             </div>
         </div>
     );
